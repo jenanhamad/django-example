b895e547405c23f6a05faf3319794e58bce112dc
jenanhamad <jenanhamad31@gmail.com>
2020-08-18 19:56:15 +0300

    first commit

---
+++ learnTemplate/basicapp/templatetags/my_extras.py
@@ -0,0 +1,9 @@
+from django import template
+register = template.Library()
+@register.filter(name='cut')
+def cut(value,arg):
+    """
+    this cuts all value of arg from string
+    """
+    return value.replace(arg,'')
+# register.filter('cut',cut)
